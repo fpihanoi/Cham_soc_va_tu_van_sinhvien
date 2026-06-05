5af0eacd030bc4a080b31fef1c8a63670690abb8
btechanoi <tkai12092025@gmail.com>
2026-06-05 16:43:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // config.js
 // Lưu trữ các thông tin cấu hình nhạy cảm hoặc cấu hình môi trường
-const supabaseUrl = 'https://qwwohuuaigmdcfiskfre.supabase.co';
+const supabaseUrl = 'https://ffctxuladsdozqchibuh.supabase.co';
 //const supabaseUrl = 'https://moahaaubepvvpkukjoun.supabase.co';
-
+//https://ffctxuladsdozqchibuh.supabase.co
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3d29odXVhaWdtZGNmaXNrZnJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1NjQ4MjYsImV4cCI6MjA5NjE0MDgyNn0.qWPBAB8mxOnbZ80d0cShANMVJVoRBlftDbmHdfUc-Z8
 // WARNING: Điền Anon Key vào thay cho process.env.SUPABASE_KEY vì file HTML chạy trên trình duyệt client
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3d29odXVhaWdtZGNmaXNrZnJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1NjQ4MjYsImV4cCI6MjA5NjE0MDgyNn0.qWPBAB8mxOnbZ80d0cShANMVJVoRBlftDbmHdfUc-Z8'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmY3R4dWxhZHNkb3pxY2hpYnVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2NDE4ODIsImV4cCI6MjA5NjIxNzg4Mn0.DkOOf4KaQs-5K2RIhkqVgniEvaV7z2bkIOtSBKBKuLE'
   //'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vYWhhYXViZXB2dnBrdWtqb3VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzMjk2NTksImV4cCI6MjA5MTkwNTY1OX0.AgY0-QhMeTXF08eCBRnIxyAjq8mtNHYQBDr6F5jqPcQ';
